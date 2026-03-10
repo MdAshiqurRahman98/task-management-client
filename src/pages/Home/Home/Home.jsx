c78cd0b0a35795c6197c797d83b31171a2a56523
MdAshiqurRahman98 <MDASHIQUROFFICIAL@gmail.com>
2026-03-10 11:46:04 +0600

    Home_screen updated

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
             </Helmet>
             <Banner></Banner>
             <div className="mt-5 mb-9">
-                <h3 className="text-3xl font-bold text-center mb-14">Who Benefits from TaskFlow</h3>
+                <h3 className="text-3xl font-bold text-center mb-14">Who Benefited from TaskFlow</h3>
                 <BenefitedPerson></BenefitedPerson>
             </div>
         </div>
